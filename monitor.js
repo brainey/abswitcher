714d565aaa0a093f3d003c7a3e850c0590ba5aca
Bill Rainey <billrainey@atieva.com>
2019-06-28 16:08:44 -0700

    Trying to add the ability for server to uplink to the PCS, also update modify monitor to catch most events.

--- monitor.js
+++ monitor.js
@@ -1,9 +1,23 @@
+#!/usr/bin/env node
+
+const args = process.argv.slice(2);
+
+if (args.length < 1) {
+    console.error('Usgae: ' + __filename + ' SERVER_ADDRESS');
+    process.exit(-1);
+}
+const address = args[0];
+
 const io = require('socket.io-client');
+const patch = require('socketio-wildcard')(io.Manager);
+
+
 //const broker = 'http://voicebox3.corp.lucid.lcl:3000';
-const broker = 'http://127.0.0.1:3000';
+//const broker = 'http://127.0.0.1:3000';
 //const broker = 'http://10.100.11.36:3000';
 
-var socket = io(broker);
+var socket = io(address);
+patch(socket);
 
 var connections = {};
 
@@ -18,6 +32,15 @@ socket.on('connect', () => {
     delete connections[id];
   });
 
+  socket.on('ping', () => {
+    console.log('Ping message sent ...');
+  });
+  socket.on('pong', (ms) => {
+    console.log('Pong response in ',ms,'ms');
+  });
+  socket.on('pcs-web-players', (x) => {
+    console.log('pcs-web-players: ', x);
+  });
   socket.on('switchui', (mode) => {
     console.log('Change mode to ', mode);
   });
@@ -26,5 +49,8 @@ socket.on('connect', () => {
     console.log('controls change ', mode);
   });
 
+  socket.on('*', (packet) => {
+    console.log('wildcard: ', packet);
+  });
 });
 
--- switchserver.js
+++ switchserver.js
@@ -4,38 +4,49 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var path = require('path');
 
+var uplinkio = require('socket.io-client');
 
 var clients = {};
 
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 io.on('connection', function(socket){
-  clients[socket.id] = socket;
+    clients[socket.id] = socket;
 
-  console.log('a user connected: ', socket.id);
-  console.log('number of clients: ', Object.keys(clients).length)
-
-  socket.on('switchui', function(msg){
-    console.log('switchui', msg);
-    io.emit('switchui', msg);
-  });
-
-  socket.on('controls', function(msg){
-    console.log('controls', msg);
-    io.emit('controls', msg);
-  });
-
-  socket.on('disconnect', function(data) {
-    console.log('socket disconnecting: ', socket.id);
-    delete clients[socket.id];
+    console.log('a user connected: ', socket.id);
     console.log('number of clients: ', Object.keys(clients).length)
+
+    socket.on('switchui', function(msg){
+	console.log('switchui', msg);
+	io.emit('switchui', msg);
+    });
+
+    socket.on('controls', function(msg){
+	console.log('controls', msg);
+	io.emit('controls', msg);
+	var message = msg.name;
+	var value = msg.value;
+	uplink.emit(message, value);
   });
+
+    socket.on('disconnect', function(data) {
+	console.log('socket disconnecting: ', socket.id);
+	delete clients[socket.id];
+	console.log('number of clients: ', Object.keys(clients).length)
+    });
+});
+
+console.log('Establishing uplink to ',uplink_url);
+var uplink = uplinkio('http://uxbuck-pcs:9981');
+uplink.on('connect', () => {
+    var id = socket.id;
+    console.log('Connected to uplink ', uplink_url, ' id ',  id);
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
